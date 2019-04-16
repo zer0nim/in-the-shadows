a13f522fd3232337098d14263c2e546cdb8e3b63
Ernest MARIN <emarin@student.42.fr>
2019-04-16 17:41:07 +0200

    Implement validity percentage help bar (hot / cold)

---
+++ Assets/Scripts/Gui.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gui : MonoBehaviour {
+	public Color coldColor = new Color(0.09f, 0.698f, 1); // #17B2FF
+	public Color hotColor = Color.red;
+
+
+	private RectTransform validPercRT;
+	private Image validPercImg;
+	void Awake () {
+		Transform validPercBar = transform.Find("ValidPercBar").GetChild(0);
+		validPercRT = validPercBar.GetComponent<RectTransform>();
+		validPercImg = validPercBar.GetComponent<Image>();
+	}
+
+	void Update () {
+		validPercRT.anchorMin = new Vector2(1 - PuzzleManager.instance.validPerc, 0);
+		validPercImg.color = Color.Lerp(coldColor, hotColor, PuzzleManager.instance.validPerc);
+	}
+}
--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 using System.Linq;
 
 public class PuzzleManager : MonoBehaviour {
+	public static PuzzleManager instance = null;
 	public List<Part> parts;
-	public int validRotAngle = 5;
+	public float minValidPerc = .95f;
+
+	[HideInInspector]
+	public float validPerc;
 
 	private List<float> validsParts;
 
 	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
 		validsParts = Enumerable.Repeat(180f, parts.Count).ToList();
 	}
 
@@ -19,7 +28,9 @@ public class PuzzleManager : MonoBehaviour {
 		for (int i = 0; i < parts.Count; ++i)
 			validsParts[i] = Quaternion.Angle(parts[i].gameObject.transform.rotation, Quaternion.Euler(parts[i].validRot));
 
-		// if (validsParts.TrueForAll(x => x < validRotAngle))
-		// 	print("You win !");
+		validPerc = 1 - validsParts.Max() / 180;
+
+		if (validPerc > minValidPerc)
+			print("Valid !");
 	}
 }

bfaec1bec8c26dce0b2f23620122c0da71a4b5ac
zer0nim <emarin@e2r12p3.42.fr>
2019-04-16 19:19:08 +0200

    Rotate on confirm

--- Assets/Scripts/Part.cs
+++ Assets/Scripts/Part.cs
@@ -8,9 +8,16 @@ public class Part : MonoBehaviour {
 	public KeyCode moveKey = KeyCode.LeftShift;
 	public int difficulty = 3;
 
-
 	private Vector3 screenSpace, offset;
 	private Vector2 mousePerc;
+
+	void Update () {
+		if (PuzzleManager.instance.finished)
+		{
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(validRot),
+			   PuzzleManager.instance.validAnimSpeed * Time.deltaTime);
+		}
+	}
 
 	// mousePerc is used to calculate rotation angle
 	Vector2 CalcMousePerc () {
@@ -21,41 +28,45 @@ public class Part : MonoBehaviour {
 	}
 
 	void OnMouseDown () {
-		screenSpace = Camera.main.WorldToScreenPoint(transform.position);
-		// calculate any difference between the puzzle world position and the mouses Screen position converted to a world point
-		offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, screenSpace.z));
-		// update mousePerc, used later to calculate rotation angle
-		mousePerc = CalcMousePerc();
+		if (!PuzzleManager.instance.finished) {
+			screenSpace = Camera.main.WorldToScreenPoint(transform.position);
+			// calculate any difference between the puzzle world position and the mouses Screen position converted to a world point
+			offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, screenSpace.z));
+			// update mousePerc, used later to calculate rotation angle
+			mousePerc = CalcMousePerc();
+		}
 	}
 
 	void OnMouseDrag () {
-		Vector2 newMousePerc = CalcMousePerc();
+		if (!PuzzleManager.instance.finished) {
+			Vector2 newMousePerc = CalcMousePerc();
 
-		// move mode
-		if (Input.GetKey(moveKey) && difficulty >= 3) {
-			// keep track of the mouse position
-			var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
-			// convert the screen mouse position to world point and adjust with offset
-			var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
-			transform.position = curPosition;
+			// move mode
+			if (Input.GetKey(moveKey) && difficulty >= 3) {
+				// keep track of the mouse position
+				var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+				// convert the screen mouse position to world point and adjust with offset
+				var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+				transform.position = curPosition;
 
-			// continue updating mousePerc to avoid a jump in the rotation when we will stop holding the moveKey
-			mousePerc = newMousePerc;
-		}
-		// vertical rotation mode
-		else if (Input.GetKey(verticalKey) && difficulty >= 2) {
-			if (mousePerc.y != newMousePerc.y) {
-				float angle = (mousePerc.y - newMousePerc.y) * 360;
-				transform.RotateAround(transform.position, Camera.main.transform.right, -angle);
+				// continue updating mousePerc to avoid a jump in the rotation when we will stop holding the moveKey
 				mousePerc = newMousePerc;
 			}
-		}
-		// horizontal rotation mode
-		else {
-			if (mousePerc.x != newMousePerc.x) {
-				float angle = (mousePerc.x - newMousePerc.x) * 360;
-				transform.RotateAround(transform.position, Camera.main.transform.up, angle);
-				mousePerc = newMousePerc;
+			// vertical rotation mode
+			else if (Input.GetKey(verticalKey) && difficulty >= 2) {
+				if (mousePerc.y != newMousePerc.y) {
+					float angle = (mousePerc.y - newMousePerc.y) * 360;
+					transform.RotateAround(transform.position, Camera.main.transform.right, -angle);
+					mousePerc = newMousePerc;
+				}
+			}
+			// horizontal rotation mode
+			else {
+				if (mousePerc.x != newMousePerc.x) {
+					float angle = (mousePerc.x - newMousePerc.x) * 360;
+					transform.RotateAround(transform.position, Camera.main.transform.up, angle);
+					mousePerc = newMousePerc;
+				}
 			}
 		}
 	}
--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -7,10 +7,12 @@ public class PuzzleManager : MonoBehaviour {
 	public static PuzzleManager instance = null;
 	public List<Part> parts;
 	public float minValidPerc = .95f;
+	public float validAnimSpeed = 10;
 
 	[HideInInspector]
+	public bool finished { get ; private set ;}
+	[HideInInspector]
 	public float validPerc;
-
 	private List<float> validsParts;
 
 	void Awake () {
@@ -19,18 +21,21 @@ public class PuzzleManager : MonoBehaviour {
 		else if (instance != this)
 			Destroy(gameObject);
 
+		finished = false;
 		validsParts = Enumerable.Repeat(180f, parts.Count).ToList();
 	}
 
 	void Update ()
 	{
-		// Store angle betwwen rotations quaternion
-		for (int i = 0; i < parts.Count; ++i)
-			validsParts[i] = Quaternion.Angle(parts[i].gameObject.transform.rotation, Quaternion.Euler(parts[i].validRot));
+		if (!finished) {
+			// Store angle betwwen rotations quaternion
+			for (int i = 0; i < parts.Count; ++i)
+				validsParts[i] = Quaternion.Angle(parts[i].gameObject.transform.rotation, Quaternion.Euler(parts[i].validRot));
 
-		validPerc = 1 - validsParts.Max() / 180;
+			validPerc = 1 - validsParts.Max() / 180;
 
-		if (validPerc > minValidPerc)
-			print("Valid !");
+			if (validPerc > minValidPerc)
+				finished = true;
+		}
 	}
 }

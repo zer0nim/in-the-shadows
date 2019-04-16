fbeae336d99e8105d33d4515cff2507b05c79f56
Ernest MARIN <emarin@student.42.fr>
2019-04-16 12:49:41 +0200

    Implement rotation validation

---
+++ Assets/Scripts/Part.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part : MonoBehaviour {
+	public Vector3 validRot;
+	public Vector3 validPos;
+
+	void Update () {
+
+	}
+}

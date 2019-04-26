02ef299b650808bdf345de73ff106bfce28de966
zer0nim <emarin@e2r13p11.42.fr>
2019-04-26 14:35:08 +0200

    Create main menu environement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CursorType {normal, link};
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+	[System.Serializable]
+	public struct CURSORS {
+		public Texture2D normal;
+		public Texture2D link;
+	}
+	public CURSORS Cursors;
+
+    private Vector2 hotSpot = Vector2.zero;
+
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+
+		// Set this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+
+		GameManager.instance.SetCursor(CursorType.normal);
+	}
+
+	public void SetCursor(CursorType cursorType) {
+		Texture2D newTexture;
+		switch (cursorType)
+        {
+            case CursorType.link:
+				newTexture = Cursors.link;
+				break;
+            case CursorType.normal:
+			default:
+                newTexture = Cursors.normal;
+				break;
+        }
+		Cursor.SetCursor(newTexture, hotSpot, CursorMode.Auto);
+	}
+}
---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void OnStoryMode () {
+		print("OnStoryMode");
+	}
+
+	public void OnTestMode () {
+		print("OnTestMode");
+	}
+
+	public void OnHover () {
+		GameManager.instance.SetCursor(CursorType.link);
+	}
+
+	public void OnHoverAway () {
+		GameManager.instance.SetCursor(CursorType.normal);
+	}
+}

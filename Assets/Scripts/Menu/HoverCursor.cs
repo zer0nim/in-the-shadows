4e5640c9e9025f2428cef443c4895861a42de51d
zer0nim <emarin@e2r13p14.42.fr>
2019-04-30 14:39:23 +0200

    load level on click

---
+++ Assets/Scripts/Menu/HoverCursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverCursor : MonoBehaviour {
+	public void OnHover () {
+		GameManager.instance.SetCursor(CursorType.link);
+	}
+
+	public void OnHoverAway () {
+		GameManager.instance.SetCursor(CursorType.normal);
+	}
+}
--- Assets/Scripts/Menu/LevelManager.cs
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
-
+	public GameObject	levelBox;
+	public float		padding = .5f;
 	void Awake () {
 		var levels = GameManager.instance.levels;
 		var levelProgess = GameManager.instance.levelProgess;
 
+		Vector3 pos = Vector3.zero;
 		for (int i = 0; i < levels.Count; i++)
 		{
-			print(levels[i] + " => " + levelProgess[i]);
+			GameObject levelBoxInst = Instantiate(levelBox, pos, Quaternion.identity);
+			pos.Set(pos.x + 1 + padding, pos.y, pos.z);
+
+			LevelInfo levelInfo = levelBoxInst.GetComponent<LevelInfo>();
+			levelInfo.sceneName = levels[i];
+			levelInfo.done = levelProgess[i];
+			levelInfo.Init();
 		}
 	}
 }

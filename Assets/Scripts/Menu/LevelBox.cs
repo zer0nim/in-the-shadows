beeb811e213ad16d7513c760326a1dabbf561b61
Ernest MARIN <emarin@student.42.fr>
2019-04-30 10:50:36 +0200

    Start working on save level

--- Assets/Scripts/Menu/LevelBox.cs
+++ Assets/Scripts/Menu/LevelBox.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LevelInfo : MonoBehaviour {
-	public string	display_name;
-	public string	l_name;
+public class LevelBox : MonoBehaviour {
+	public string	displayName;
+	public string	lName;
 	public Text		textComponent;
 	public bool		done = false;
 
 	void Awake () {
-		textComponent.text = display_name;
+		textComponent.text = displayName;
 		GetComponent<Renderer>().material = done ? GameManager.instance.levelDoneMaterial : GameManager.instance.levelTodoMaterial;
 	}
 }
---
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	void Awake () {
+		var levels = GameManager.instance.levels;
+		var levelProgess = GameManager.instance.levelProgess;
+
+		for (int i = 0; i < levels.Count; i++)
+		{
+			print(levels[i] + " => " + levelProgess[i]);
+		}
+	}
+}

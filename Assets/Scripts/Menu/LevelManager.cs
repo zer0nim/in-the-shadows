8e04433e7aab4dc941aa61fb7e71f22cfc47d847
Ernest MARIN <emarin@student.42.fr>
2019-04-30 18:45:02 +0200

    Navigate between level on border hover

--- Assets/Scripts/Menu/LevelManager.cs
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour {
 	public GameObject	levelBox;
 	public float		padding = .5f;
+	[Range(0, .5f)]
+	public float		scrollWidth = .2f;
+	public float		minScrollSpeed = 2;
+	public float		maxScrollSpeed = 8;
+
+	private float xVel = 0;
 	void Awake () {
 		var levels = GameManager.instance.levels;
 		var levelProgess = GameManager.instance.levelProgess;
@@ -19,6 +25,29 @@ public class LevelManager : MonoBehaviour {
 			levelInfo.sceneName = levels[i];
 			levelInfo.done = levelProgess[i];
 			levelInfo.Init();
+
+			levelBoxInst.transform.parent = transform;
+		}
+	}
+
+	void Update () {
+		float mousePerc = Input.mousePosition.x / Screen.width;
+		mousePerc = mousePerc < 0 ? 0 : (mousePerc > 1 ? 1 : mousePerc);
+
+		xVel = 0;
+		if (mousePerc < scrollWidth)
+			xVel = -(1 - mousePerc / scrollWidth);
+		else if (mousePerc > 1 - scrollWidth)
+			xVel = 1 - (1 - mousePerc) / scrollWidth;
+
+		if (xVel < 0 && transform.GetChild(0).transform.position.x < 0) {
+			float moveX = Mathf.Lerp(minScrollSpeed, maxScrollSpeed, Mathf.Abs(xVel));
+			transform.position = new Vector3(transform.position.x + moveX * Time.deltaTime, transform.position.y, transform.position.z);
+		}
+
+		if (xVel > 0 && transform.GetChild(transform.childCount - 1).transform.position.x > 0) {
+			float moveX = - Mathf.Lerp(minScrollSpeed, maxScrollSpeed, Mathf.Abs(xVel));
+			transform.position = new Vector3(transform.position.x + moveX * Time.deltaTime, transform.position.y, transform.position.z);
 		}
 	}
 }

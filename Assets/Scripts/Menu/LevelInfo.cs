5fd9af482a8bf8e5a5acedb94a58e0a6357e3529
Ernest MARIN <emarin@student.42.fr>
2019-05-02 23:19:10 +0200

    Start to implement level menu animations

--- Assets/Scripts/Menu/LevelInfo.cs
+++ Assets/Scripts/Menu/LevelInfo.cs
@@ -7,25 +7,38 @@ using UnityEngine.SceneManagement;
 public enum LevelStatus { Locked, Unlocked, Done }
 
 public class LevelInfo : HoverCursor {
-	public string	sceneName;
-	public Text		textComponent;
+	public string		sceneName;
+	public Text			textComponent;
+	public Image		textFillBox;
 	public LevelStatus	status = LevelStatus.Locked;
-
+	public float		lockedAlpha = .4f;
+	public Animator		animator;
 	void Awake () {
 		Init();
 	}
 
 	public void Init () {
 		textComponent.text = sceneName;
-		GetComponent<Renderer>().material = status == LevelStatus.Done ? GameManager.instance.levelDoneMaterial : GameManager.instance.levelTodoMaterial;
-        if (status == LevelStatus.Locked)
-            Tools.SetMaterialAlpha(GetComponent<Renderer>(), .4f);
+		SetMaterial(status == LevelStatus.Done ? 1 : 0);
 		active = status != LevelStatus.Locked;
+		Tools.SetMaterialAlpha(GetComponent<Renderer>(), active ? 1 : lockedAlpha);
+		animator.SetBool("Unlocked", active);
+	}
+
+	// Used to set material alpha from animation event
+	public void SetMaterialAlpha(float alpha) {
+		Tools.SetMaterialAlpha(GetComponent<Renderer>(), alpha);
+	}
+	// Used to set material alpha from animation event, int used because bool is not suported...
+	public void SetMaterial(int done) {
+		GetComponent<Renderer>().material = done == 1 ? GameManager.instance.levelDoneMaterial : GameManager.instance.levelTodoMaterial;
 	}
 
 	public void OnClick () {
 		if (active)
-			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+			animator.SetTrigger("Done");			// SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		else
+			animator.SetTrigger("Unlock");
 	}
 
 	void OnMouseDown () { OnClick(); }

5e726b5de0181c5a26b24ae48d0d927a47fbb832
Ernest MARIN <emarin@e1r4p16.42.fr>
2019-07-03 13:06:04 +0200

    implement confirmation box

--- Assets/Scripts/Menu/DeleteProgress.cs
+++ Assets/Scripts/Menu/DeleteProgress.cs
@@ -6,10 +6,30 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class DeleteProgress : HoverCursor {
-	public void OnDeleteProgress () {
-		GameManager.instance.SetCursor(CursorType.normal);
-		GameManager.instance.initProgress();
-		GameManager.instance.SaveGame();
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	public Dialog dialog = null;
+
+	void Awake () {
+		if (dialog == null) Debug.LogError("You need to asign \"dialog\" to the DeleteProgress script !");
 	}
+
+	public void OnDeleteProgress () {
+		StopCoroutine("DelProgressCoroutine");
+		StartCoroutine("DelProgressCoroutine");
+	}
+
+	IEnumerator DelProgressCoroutine () {
+		dialog.setVisible();
+
+		while (dialog.result == DialogResult.none)
+			yield return null;
+
+		if (dialog.result == DialogResult.ok) {
+			GameManager.instance.SetCursor(CursorType.normal);
+			GameManager.instance.initProgress();
+			GameManager.instance.SaveGame();
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+
+		dialog.result = DialogResult.none;
+    }
 }
---
+++ Assets/Scripts/Menu/Dialog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DialogResult {none, ok, cancel};
+
+public class Dialog : HoverCursor {
+	public GameObject dialogBg = null;
+	[HideInInspector]
+	public	DialogResult result = DialogResult.none;
+
+	void Awake () {
+		if (dialogBg == null) Debug.LogError("You need to asign \"DialogBg\" to the Dialog script !");
+	}
+
+	public void setVisible(bool visible = true) {
+		dialogBg.SetActive(visible);
+	}
+
+	public void OnOk () {
+		result = DialogResult.ok;
+		setVisible(false);
+	}
+
+	public void OnCancel () {
+		result = DialogResult.cancel;
+		setVisible(false);
+	}
+}

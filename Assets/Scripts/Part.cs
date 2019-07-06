4e14dc77828914c6a6bcdfc35e75273fc9cce470
Ernest MARIN <marinernes@hotmail.fr>
2019-07-06 19:14:24 +0200

    Add posibility to only move in one axis

--- Assets/Scripts/Part.cs
+++ Assets/Scripts/Part.cs
@@ -8,14 +8,18 @@ public class Part : MonoBehaviour {
 	public KeyCode verticalKey = KeyCode.LeftControl;
 	public KeyCode moveKey = KeyCode.LeftShift;
 	public int	difficulty = 3;
+	public bool	moveHori = false;
+	public bool	moveVert = false;
 	[Header("v-- left, top, right, bot => in % of windows --v")]
 	// windows percentage where we are allowed to move
 	public Vector4	moveZone = new Vector4(.2f, .2f, .2f, .2f);
+	[HideInInspector]
 	public bool	winAnimFinished = false;
 
 	private Vector4 moveZoneCoord;
 	private Vector3 screenSpace, offset;
 	private Vector2 mousePerc;
+	private Vector3 startScreenSpace;
 
 	void Start () {
 		moveZoneCoord = new Vector4(
@@ -51,6 +55,8 @@ public class Part : MonoBehaviour {
 			offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, screenSpace.z));
 			// update mousePerc, used later to calculate rotation angle
 			mousePerc = CalcMousePerc();
+
+			startScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
 		}
 	}
 
@@ -60,21 +66,30 @@ public class Part : MonoBehaviour {
 
 			// move mode
 			if (Input.GetKey(moveKey) && difficulty >= 3) {
-				// keep track of the mouse position
-				var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+				if (moveHori || moveVert) {
+					// keep track of the mouse position
+					Vector3 curScreenSpace = startScreenSpace;
 
-				// limit the moves to moveZone
-				curScreenSpace.x = curScreenSpace.x < moveZoneCoord.x ? moveZoneCoord.x : curScreenSpace.x;
-				curScreenSpace.x = curScreenSpace.x > moveZoneCoord.z ? moveZoneCoord.z : curScreenSpace.x;
-				curScreenSpace.y = curScreenSpace.y > moveZoneCoord.y ? moveZoneCoord.y : curScreenSpace.y;
-				curScreenSpace.y = curScreenSpace.y < moveZoneCoord.w ? moveZoneCoord.w : curScreenSpace.y;
+					if (moveHori && moveVert)
+						curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+					else if (moveHori)
+						curScreenSpace = new Vector3(Input.mousePosition.x, startScreenSpace.y, screenSpace.z);
+					else if (moveVert)
+						curScreenSpace = new Vector3(startScreenSpace.x, Input.mousePosition.y, screenSpace.z);
 
-				// convert the screen mouse position to world point and adjust with offset
-				var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
-				transform.position = curPosition;
+					// limit the moves to moveZone
+					curScreenSpace.x = curScreenSpace.x < moveZoneCoord.x ? moveZoneCoord.x : curScreenSpace.x;
+					curScreenSpace.x = curScreenSpace.x > moveZoneCoord.z ? moveZoneCoord.z : curScreenSpace.x;
+					curScreenSpace.y = curScreenSpace.y > moveZoneCoord.y ? moveZoneCoord.y : curScreenSpace.y;
+					curScreenSpace.y = curScreenSpace.y < moveZoneCoord.w ? moveZoneCoord.w : curScreenSpace.y;
 
-				// continue updating mousePerc to avoid a jump in the rotation when we will stop holding the moveKey
-				mousePerc = newMousePerc;
+					// convert the screen mouse position to world point and adjust with offset
+					var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+					transform.position = curPosition;
+
+					// continue updating mousePerc to avoid a jump in the rotation when we will stop holding the moveKey
+					mousePerc = newMousePerc;
+				}
 			}
 			// vertical rotation mode
 			else if (Input.GetKey(verticalKey) && difficulty >= 2) {

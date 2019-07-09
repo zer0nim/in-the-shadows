ec491c7e05f4df5c525a0b64cab8ef130b014959
Ernest MARIN <emarin@e1r4p1.42.fr>
2019-07-09 13:26:11 +0200

    Create test mode

--- Assets/Scripts/Menu/LevelManager.cs
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -11,36 +11,58 @@ public class LevelManager : MonoBehaviour {
 	public float		minScrollSpeed = 2;
 	public float		maxScrollSpeed = 8;
 	public KeyCode		mainMenuKey = KeyCode.Escape;
+	public GameObject	deleteProgress = null;
 
 	private float xVel = 0;
 	void Awake () {
+		if (deleteProgress == null)
+			Debug.LogError("You need to set deleteProgress to LevelManager script !");
+
 		var levels = GameManager.instance.levels;
 
-		bool playSuccess = false;
-		Vector3 pos = Vector3.zero;
-		for (int i = 0; i < levels.Count; i++)
-		{
-			GameObject levelBoxInst = Instantiate(levelBox, pos, Quaternion.identity);
-			pos.Set(pos.x + 1 + padding, pos.y, pos.z);
+		if (!GameManager.instance.testMode) {
+			bool playSuccess = false;
+			Vector3 pos = Vector3.zero;
+			for (int i = 0; i < levels.Count; i++)
+			{
+				GameObject levelBoxInst = Instantiate(levelBox, pos, Quaternion.identity);
+				pos.Set(pos.x + 1 + padding, pos.y, pos.z);
 
-			LevelInfo levelInfo = levelBoxInst.GetComponent<LevelInfo>();
-			levelInfo.sceneName = levels[i];
-			levelInfo.status = GameManager.instance.save.levelProgess[i];
-			levelInfo.animationDone = GameManager.instance.save.animationDone[i];
-			if (!GameManager.instance.save.animationDone[i])
-				playSuccess = true;
+				LevelInfo levelInfo = levelBoxInst.GetComponent<LevelInfo>();
+				levelInfo.sceneName = levels[i];
+				levelInfo.status = GameManager.instance.save.levelProgess[i];
+				levelInfo.animationDone = GameManager.instance.save.animationDone[i];
+				if (!GameManager.instance.save.animationDone[i])
+					playSuccess = true;
 
-			GameManager.instance.save.animationDone[i] = true;
-			levelInfo.Init();
+				GameManager.instance.save.animationDone[i] = true;
+				levelInfo.Init();
 
-			levelBoxInst.transform.parent = transform;
+				levelBoxInst.transform.parent = transform;
+			}
+
+			if (playSuccess)
+				AudioManager.instance.Play("unlock");
+
+			GameManager.instance.SaveGame();
+		} else {
+			deleteProgress.SetActive(false);
+
+			Vector3 pos = Vector3.zero;
+			for (int i = 0; i < levels.Count; i++)
+			{
+				GameObject levelBoxInst = Instantiate(levelBox, pos, Quaternion.identity);
+				pos.Set(pos.x + 1 + padding, pos.y, pos.z);
+
+				LevelInfo levelInfo = levelBoxInst.GetComponent<LevelInfo>();
+				levelInfo.sceneName = levels[i];
+				levelInfo.status = LevelStatus.Unlocked;
+				levelInfo.animationDone = true;
+				levelInfo.Init();
+
+				levelBoxInst.transform.parent = transform;
+			}
 		}
-
-		if (playSuccess)
-			AudioManager.instance.Play("unlock");
-
-
-		GameManager.instance.SaveGame();
 	}
 
 	void Update () {

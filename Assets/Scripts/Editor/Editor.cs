62b1b69d0363d9799604af7ae6a272004a0ca273
Ernest MARIN <emarin@student.42.fr>
2019-07-09 14:04:38 +0200

    Edit build settings

---
+++ Assets/Scripts/Editor/Editor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Editor : MonoBehaviour {
+
+	[MenuItem("Edit/Reset Playerprefs")]
+	public static void DeletePlayerPrefs()
+	{
+		PlayerPrefs.DeleteAll();
+	}
+}
